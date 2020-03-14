a6619415bc35d2942554bea602b731c47a58cf7e
F4RB3R <53954664+f4rber@users.noreply.github.com>
2020-03-14 11:10:36 +0200

    Update GenerateKey.py

--- GenerateKey.py
+++ GenerateKey.py
@@ -1,6 +1,26 @@
 from cryptography.fernet import Fernet
-key = Fernet.generate_key()
+logo = r'''
+▄ •▄ ▄▄▄ . ▄· ▄▌     ▄▄ • ▄▄▄ . ▐ ▄ ▄▄▄ .▄▄▄   ▄▄▄· ▄▄▄▄▄      ▄▄▄  
+█▌▄▌▪▀▄.▀·▐█▪██▌    ▐█ ▀ ▪▀▄.▀·•█▌▐█▀▄.▀·▀▄ █·▐█ ▀█ •██  ▪     ▀▄ █·
+▐▀▀▄·▐▀▀▪▄▐█▌▐█▪    ▄█ ▀█▄▐▀▀▪▄▐█▐▐▌▐▀▀▪▄▐▀▀▄ ▄█▀▀█  ▐█.▪ ▄█▀▄ ▐▀▀▄ 
+▐█.█▌▐█▄▄▌ ▐█▀·.    ▐█▄▪▐█▐█▄▄▌██▐█▌▐█▄▄▌▐█•█▌▐█ ▪▐▌ ▐█▌·▐█▌.▐▌▐█•█▌
+·▀  ▀ ▀▀▀   ▀ •     ·▀▀▀▀  ▀▀▀ ▀▀ █▪ ▀▀▀ .▀  ▀ ▀  ▀  ▀▀▀  ▀█▄▀▪.▀  ▀                                                                  
+'''
 
-file = open('key.key', 'wb')  # Открываем файл / Open file
-file.write(key)  # Записываем ключ в файл / Write key to the file
-file.close()
+print(logo)
+
+try:
+    key = Fernet.generate_key()  # Переменная с ключём
+
+    file = open('key.key', 'wb')  # Открываем файл / Open file
+    file.write(key)  # Записываем ключ в файл / Write key to the file
+    file.close()  # Закрываем файл / Close the file
+
+    # Если всё прошло успешно пишем что ключ сгенерирован
+    # If everything is ok print message
+    print("KEY GENERATED SUCCESSFULLY")
+
+except Exception as ex:
+    # Если что-то пошло не так пишем сообщение
+    # If something goes wrong print message
+    print("KEY WASN`T GENERATED SUCCESSFULLY\n" + str(ex))
