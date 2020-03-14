0dfd4480340fc285f76f3c9ff048547d735be9fd
F4RB3R <53954664+f4rber@users.noreply.github.com>
2020-03-14 15:28:34 +0200

    Update GenerateKey.py

--- GenerateKey.py
+++ GenerateKey.py
@@ -1,4 +1,6 @@
 from cryptography.fernet import Fernet
+from Cryptodome.PublicKey import RSA
+
 logo = r'''
 ▄ •▄ ▄▄▄ . ▄· ▄▌     ▄▄ • ▄▄▄ . ▐ ▄ ▄▄▄ .▄▄▄   ▄▄▄· ▄▄▄▄▄      ▄▄▄  
 █▌▄▌▪▀▄.▀·▐█▪██▌    ▐█ ▀ ▪▀▄.▀·•█▌▐█▀▄.▀·▀▄ █·▐█ ▀█ •██  ▪     ▀▄ █·
@@ -9,6 +11,7 @@ logo = r'''
 
 print(logo)
 
+# Fernet
 try:
     key = Fernet.generate_key()  # Переменная с ключём
 
@@ -24,3 +27,19 @@ except Exception as ex:
     # Если что-то пошло не так пишем сообщение
     # If something goes wrong print message
     print("KEY WASN`T GENERATED SUCCESSFULLY\n" + str(ex))
+
+# AES
+try:
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    file_out = open("private.pem", "wb")
+    file_out.write(private_key)
+    print("private_key")
+
+    public_key = key.publickey().export_key()
+    file_out = open("receiver.pem", "wb")
+    file_out.write(public_key)
+    print("public_key")
+except Exception as ex:
+    print("KEY WASN`T GENERATED SUCCESSFULLY\n" + str(ex))
+
