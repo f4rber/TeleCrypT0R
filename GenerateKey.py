d32d64951202da6b3a658de8df4a7a19ad9f1384
F4RB3R <53954664+f4rber@users.noreply.github.com>
2020-11-02 02:33:06 +0200

    Update GenerateKey.py

--- GenerateKey.py
+++ GenerateKey.py
@@ -13,19 +13,17 @@ print(logo)
 
 # Fernet
 try:
-    key = Fernet.generate_key()  # Переменная с ключём
+    key = Fernet.generate_key()
 
-    file = open('key.key', 'wb')  # Открываем файл / Open file
-    file.write(key)  # Записываем ключ в файл / Write key to the file
-    file.close()  # Закрываем файл / Close the file
+    file = open('key.key', 'wb')
+    file.write(key)
+    file.close()
 
     # Если всё прошло успешно пишем что ключ сгенерирован
     # If everything is ok print message
     print("KEY GENERATED SUCCESSFULLY")
 
 except Exception as ex:
-    # Если что-то пошло не так пишем сообщение
-    # If something goes wrong print message
     print("KEY WASN`T GENERATED SUCCESSFULLY\n" + str(ex))
 
 # AES
